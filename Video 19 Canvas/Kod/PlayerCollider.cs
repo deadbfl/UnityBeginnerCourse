c209fb3e153b39e9f53f11c68dc62335fe431f52
deadbfl <ahmetemreyildiz51@gmail.com>
2023-04-09 12:57:04 +0300

    19 updated

---
+++ Video 19 Canvas/Kod/PlayerCollider.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollider : MonoBehaviour
+{
+    [SerializeField] private GameObject customerPanel;
+    private void OnTriggerEnter(Collider other)
+    {
+         if(other.CompareTag("CustomerArea"))
+        {
+            customerPanel.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("CustomerArea"))
+        {
+            customerPanel.SetActive(false);
+        }
+    }
+}

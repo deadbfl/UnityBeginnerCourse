e5e66c43c3bb17666dd29a47a4ac04a55ce8b5cb
Ahmet Emre YILDIZ <ahmetemreyildiz51@gmail.com>
2023-09-04 12:56:51 +0300

    Video 23 Eklendi

---
+++ Video 23 GetComponent/Customer.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer : MonoBehaviour
+{
+    public GameObject player;
+
+    [SerializeField] private GameObject[] foods;
+    [SerializeField] private bool customerCheck = false;
+
+    [SerializeField] private int index; // Yemek Tipini Tuttugumuz Veri
+    private int lastIndex = -1;
+
+    private void OnEnable()
+    {
+        OpenCustomer();
+    }
+    private void OnDisable()
+    {
+        if (customerCheck == false)
+        {
+            foods[index].SetActive(false);
+        }
+    }
+
+    public void OpenCustomer()
+    {
+        if (!customerCheck) // !customerCheck ile customerCheck == false birbiri ile ayni islevi gorur
+        {
+            do
+            {
+                index = Random.Range(0, foods.Length); // Yemegimi secti
+            } while (index == lastIndex);
+
+            lastIndex = index;
+            foods[index].SetActive(true);
+            customerCheck = true;
+        }
+    }
+    public void RejectCustomer()
+    {
+        if (customerCheck) // customerCheck ile customerCheck == true birbiri ile ayni islevi gorur
+        {
+            foods[index].SetActive(false);
+            customerCheck = false;
+        }
+        OpenCustomer();
+    }
+
+    public void AcceptCustomer()
+    {
+        if(customerCheck)
+        {
+            //print(foods[index].name);   
+            player.GetComponent<PlayerFoodType>().foodType = index;
+        }
+    }
+}

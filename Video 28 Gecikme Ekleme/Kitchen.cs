8d633e7df67243162420a95fe767456be79cb6f8
Ahmet Emre YILDIZ <ahmetemreyildiz51@gmail.com>
2023-09-09 18:52:21 +0300

    Video 28 Eklendi

---
+++ Video 28 Gecikme Ekleme/Kitchen.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kitchen : MonoBehaviour
+{
+    [SerializeField] private GameObject[] foods;
+    [SerializeField] private Transform spawnPoint;
+
+    public int foodType;
+    public void CreateFood()
+    {
+        Vector3 firstFoodPosition = foods[foodType].transform.position;
+        Instantiate(foods[foodType],spawnPoint.position + firstFoodPosition,Quaternion.identity);
+    }
+
+    private IEnumerator FoodCoroutine()
+    {
+        yield return new WaitForSeconds(5);
+        CreateFood();
+    }
+
+    public void GiveMeFood()
+    {
+        StartCoroutine(FoodCoroutine());
+    }
+}

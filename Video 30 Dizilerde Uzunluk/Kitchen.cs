dd46181f58ea8a8f95e3ea3cd458f837eeef9417
Ahmet Emre YILDIZ <ahmetemreyildiz51@gmail.com>
2023-09-11 17:37:06 +0300

    Video 30 Eklendi

---
+++ Video 30 Dizilerde Uzunluk/Kitchen.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kitchen : MonoBehaviour
+{
+    [SerializeField] private GameObject[] foods;
+    [SerializeField] private Transform spawnPoint;
+
+    public int foodType;
+    public void CreateFood()
+    {
+        foodType = PlayerFoodType.instance.foodType;
+
+        if (0 <= foodType && foodType < foods.Length)
+        {
+            Vector3 firstFoodPosition = foods[foodType].transform.position;
+            Instantiate(foods[foodType], spawnPoint.position + firstFoodPosition, Quaternion.identity);
+        }
+    }
+
+    private IEnumerator FoodCoroutine()
+    {
+        yield return new WaitForSeconds(5);
+        CreateFood();
+    }
+
+    public void GiveMeFood()
+    {
+        StartCoroutine(FoodCoroutine());
+    }
+}
